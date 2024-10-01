561d985f3d0c062834c1446e3d8bfb7dfc0c3bd3
Gaurav Kumar <gaurav31120@gmail.com>
2024-10-01 08:47:43 +0530

    4 -> 1 - Modules System, FS, REPL, NPM, Package.json ---- checking the performance time

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const lib = require("./lib.js");
 // import {sum,diff} from './lib.js'
 const fs = require("fs");
 
-const txt = fs.readFileSync('demo.txt','utf-8')
-console.log(txt)
+
+const t1 = performance.now()
+const txt = fs.readFileSync('demo.txt','utf-8') // synchronous
+// fs.readFile("demo.txt", "utf-8", (err,txt) => {        // asynchronous
+//   console.log(txt);
+// });
+console.log(txt);
 
 console.log(lib.sum(4, 5), lib.diff(3, 6));
+const t2 = performance.now()
+console.log(t2-t1)
 
 const a = 5;
