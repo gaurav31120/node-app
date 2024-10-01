63587470d80669385aaa4df735cb6359e1f38e63
Gaurav Kumar <gaurav31120@gmail.com>
2024-10-01 21:25:06 +0530

    11-> 2. Web Server using Node JS ---- rendering json data in screen

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const http = require("http");
 
 const fs = require("fs");
 const index = fs.readFileSync("index.html", "utf-8");
-const data = { age: 5 };
+const data = fs.readFileSync("data.json", "utf-8");
+// const data = { age: 5 };
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
   console.log("server started");
   res.setHeader("Dummy", "DummyValue");
+  //   res.setHeader("Content-Type", "text/html");
   res.setHeader("Content-Type", "application/json");
   //   res.end(JSON.stringify(data))
-  res.end(index);
+  //   res.end(index);
+  res.end(data);
 });
 
 server.listen(8080);
