8097db351453362dd4c10d69fc677ca25514ebfe
Gaurav Kumar <gaurav31120@gmail.com>
2024-10-01 21:19:22 +0530

    10-> 2. Web Server using Node JS ---- running html file at server and creating data.json

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const http = require("http");
-const data = {age:5}
+
+const fs = require("fs");
+const index = fs.readFileSync("index.html", "utf-8");
+const data = { age: 5 };
 
 const server = http.createServer((req, res) => {
+  console.log(req.url);
   console.log("server started");
-  res.setHeader('Dummy', 'DummyValue')
-  res.setHeader('Content-Type','application/json')
-  res.end(JSON.stringify(data))
+  res.setHeader("Dummy", "DummyValue");
+  res.setHeader("Content-Type", "application/json");
+  //   res.end(JSON.stringify(data))
+  res.end(index);
 });
 
 server.listen(8080);
