3d6e0a1f9fd55d956ef4f772cca134215574c818
Gaurav Kumar <gaurav31120@gmail.com>
2024-10-01 08:14:08 +0530

    running nodejs function by es6 js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const lib = require('./lib.js')
+// const lib = require('./lib.js')
 
-console.log(lib.sum(4,5),lib.diff(3,6))
+import {sum,diff} from './lib.js'
+
+console.log(sum(4,5),diff(3,6))
 
 const a = 5;
 
