11b9e571b498c86365a75869996f949a67eade1c
Gaurav Kumar <gaurav31120@gmail.com>
2024-10-02 07:25:16 +0530

    13-> 2. Web Server using Node JS ---- showing dynamic HTML data

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const http = require("http");
 
 const fs = require("fs");
 const index = fs.readFileSync("index.html", "utf-8");
-const data = fs.readFileSync("data.json", "utf-8");
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 // const data = { age: 5 };
+const product = data.products[0];
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
@@ -14,18 +15,24 @@ const server = http.createServer((req, res) => {
       break;
     case "/api":
       res.setHeader("Content-Type", "application/json");
-      res.end(data);
+      res.end(JSON.stringify(data));
       break;
     case "/product":
       res.setHeader("Content-Type", "text/html");
-      res.end(index);
+      let modifiedIndex = index
+        .replace("**title**", product.title)
+        .replace("**url**", product.thumbnail)
+        .replace("**price**", product.price)
+        .replace("**rating**", product.rating);
+      // .replace("**title**", product.title);
+      res.end(modifiedIndex);
       break;
     default:
       res.writeHead(404, "NOT FOUND");
       res.end();
   }
   console.log("server started");
-//   res.setHeader("Dummy", "DummyValue");
+  //   res.setHeader("Dummy", "DummyValue");
 
   //   res.end(data);
 });
